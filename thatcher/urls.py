771498ee363bda9e508d7e7f546e82e3c36f51ba
nuriyevn <nusrat@27tech.co>
2019-04-28 20:37:07 +0300

    fix settings 2

--- thatcher/urls.py
+++ thatcher/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 
 from django.conf.urls import url, include
+from django.conf import settings
 
 urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'order/', include('dish.urls')),
 
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
